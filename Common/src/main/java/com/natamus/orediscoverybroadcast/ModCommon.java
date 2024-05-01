fe66e861ce3eaeaa614121a4ffc0adb7d25aff29
Rick South <rick@natamus.com>
2024-05-01 16:34:51 +0200

    Ore Discovery Broadcast source code for 1.20.6.

---
+++ Common/src/main/java/com/natamus/orediscoverybroadcast/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.orediscoverybroadcast;
+
+import com.natamus.orediscoverybroadcast.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}
