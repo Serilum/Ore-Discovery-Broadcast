6aae3258e5d6d853643a11919eb89cca3beb4872
Rick South <rick@natamus.com>
2024-06-14 05:20:07 +0200

    Ore Discovery Broadcast source code for 1.21.0.

---
+++ Common/src/main/java/com/natamus/orediscoverybroadcast/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.orediscoverybroadcast.util;
+
+public class Reference {
+	public static final String MOD_ID = "orediscoverybroadcast";
+	public static final String NAME = "Ore Discovery Broadcast";
+	public static final String VERSION = "2.1";
+	public static final String ACCEPTED_VERSIONS = "[1.21.0]";
+}
