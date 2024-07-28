7a9b4ba65a00d0223899a4e39512ea94dc1af485
Rick South <rick@natamus.com>
2024-07-28 07:22:40 +0200

    (v2.2) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Forge/src/main/java/com/natamus/orediscoverybroadcast/ModForge.java
+++ Forge/src/main/java/com/natamus/orediscoverybroadcast/ModForge.java
@@ -1,6 +1,7 @@
 package com.natamus.orediscoverybroadcast;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.orediscoverybroadcast.forge.config.IntegrateForgeConfig;
 import com.natamus.orediscoverybroadcast.forge.events.ForgeMiningEvent;
 import com.natamus.orediscoverybroadcast.util.Reference;
@@ -15,6 +16,10 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModForge {
 	
 	public ModForge() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 		modEventBus.addListener(this::loadComplete);
 
