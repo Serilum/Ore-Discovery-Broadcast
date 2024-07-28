1e988e4e2d5c6d14c6a34a09dd2eb38f799784a4
Rick South <rick@natamus.com>
2024-07-28 07:22:44 +0200

    (v2.2) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- NeoForge/src/main/java/com/natamus/orediscoverybroadcast/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/orediscoverybroadcast/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.orediscoverybroadcast;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.orediscoverybroadcast.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.orediscoverybroadcast.neoforge.events.NeoForgeMiningEvent;
 import com.natamus.orediscoverybroadcast.util.Reference;
@@ -15,6 +16,10 @@ import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();
